9a39d88e0676d7456e4171314cb35c6caf4c7239
Arthur Freitas <arthurfreitas.java@gmail.com>
2026-04-17 21:15:08 -0300

    terminei todos os endpointers do Autor

--- src/main/java/com/projetobiblioteca/GestaoBiblioteca/Controllers/AutorController.java
+++ src/main/java/com/projetobiblioteca/GestaoBiblioteca/Controllers/AutorController.java
@@ -7,12 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/autor")
 public class AutorController {
 
-    @Autowired
     AutorRepository autorRepository;
+
+    public AutorController(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
 
     @PostMapping("/criar")
     public ResponseEntity<AutorRequestDots> criarAutor(@RequestBody AutorRequestDots dto) {
@@ -23,5 +29,27 @@ public class AutorController {
 
         autorRepository.save(autorM);
         return ResponseEntity.ok().body(dto);
+    }
+    @GetMapping()
+    public ResponseEntity<List<AutorModel>> mostarAutores() {
+        List<AutorModel> listAutor =  autorRepository.findAll();
+        return ResponseEntity.ok(listAutor);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<AutorRequestDots> atualizarAutor(@RequestBody AutorRequestDots dto, @PathVariable UUID id) {
+        return autorRepository.findById(id).map(autorEncontrado -> {
+                autorEncontrado.setNome(dto.nome());
+                autorEncontrado.setNacionalidade(dto.nacionalidade());
+                autorRepository.save(autorEncontrado);
+
+                return ResponseEntity.ok(dto);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<AutorModel> deletarAutor(@PathVariable UUID id) {
+        return autorRepository.findById(id).map(autorEncontrado -> {
+            autorRepository.delete(autorEncontrado);
+            return ResponseEntity.ok(autorEncontrado);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
